b4632dd6b99c76351e626e953c26961bb2fe017f
Thaahir <thaahirahmad2001@gmail.com>
2025-04-08 18:45:50 +0530

    test mukta comit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author='Thaahir Ahmad',
+    author_email='thaahirahmad2001@gmail.com',
+    packages=find_packages(),
+    install_require=[]
+)
